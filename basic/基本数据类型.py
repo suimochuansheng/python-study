45dc615215afc025d1ac8de02f49a7117cc519ea
Your Name <you@example.com>
2025-10-22 07:07:36 +0800

    继续更新列表内容

---
+++ basic/基本数据类型.py
@@ -0,0 +1,42 @@
+# 一，列表
+list = ['red', 'green', 'blue', 'yellow', 'white', 'black']
+
+# 1.1 下标位输出
+# print( list[0] )
+# print( list[1] )
+# print( list[2] )
+
+# 1.2 切片输出--包含开始位，不包含结束位
+# print( list[0:3] )
+# print( list[1:4] )
+# print( list[2:] )
+# print( list[:3] )
+
+# 1.3 倒序/间隔取值
+# print(list[::-1]) # 倒序输出
+# print(list[0:6:2]) # 从0开始，每隔2个取一个，间隔2是包括左侧的元素
+
+# 
+print( list[::-2] ) # >>> ['black', 'yellow', 'green']
+print( list[5::-2] )
+
+# print( list[0::-2] ) # ['red']
+
+# 1.4 步长输出--默认步长为1，也可以自定义步长
+# print( list[5::-2] ) # 
+# print( list[1:5:2] )
+# print( list[2:5:2] )
+# print( list[3:5:2] )
+
+
+# 元组
+
+
+# 字典
+
+
+# 集合
+
+
+
+#数字，字符串，布尔值
